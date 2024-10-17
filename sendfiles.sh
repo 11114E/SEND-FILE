c44bfd2d5ef0195759d343217b45601e2aaf0155
11114E <163570670+11114E@users.noreply.github.com>
2024-10-17 09:20:55 +0400

    Add files via upload

---
+++ sendfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REMOTE_HOSTS=("172.16.6.16" "192.168.1.101" "192.168.1.102")
+LOCAL_REPO="/home/user/node-2.0.0.4-linux-amd64"
+REMOTE_PATH="/home/user/"
+
+for HOST in "${REMOTE_HOSTS[@]}"; do
+    sudo scp -r $LOCAL_REPO john@$HOST:$REMOTE_PATH
+done
